29d60601bb954e13576fb3c79893ca5e417bbaac
koziscool <kozjob@yahoo.com>
2016-07-11 13:04:10 -0500

    first cut stock panel

--- app/app.js
+++ app/app.js
@@ -1,1 +1,34 @@
 var fido = angular.module('fideligard', ['ui.router']);
+
+
+fido.config( function( $stateProvider, $urlRouterProvider) {
+
+ /// config here
+  $urlRouterProvider.otherwise('/stocks');
+
+  $stateProvider
+
+  .state( 'stocks', {
+    url: '/stocks',
+    controller: 'StocksCtrl',
+    templateUrl: '/app/templates/stocks.html',
+  })
+
+ .state('stocks.portfolio', {
+    url: '/portfolio',
+    controller: 'PortfolioCtrl',
+    templateUrl: "/app/templates/portfolio.html",
+ })
+
+ .state( 'stocks.trade', {
+    url: '/trade',
+    controller: 'TradeCtrl',
+    template: "<div>Trade</div>",
+ })
+
+ .state( 'stocks.transactions', {
+    url: '/transactions',
+    controller: 'TransactionCtrl',
+    template:  "<div>Transaction</div>",
+ });
+});
